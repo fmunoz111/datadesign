177fdc7e6e40d1e1d8d7d17ff565a41547653fa0
felizmunoz <fmunoz273@gmail.com>
2018-10-18 10:48:04 -0600

    this is my second commit

--- php/classes/foo.php
+++ php/classes/foo.php
@@ -19,30 +19,35 @@ class Patient {
 	/**
 	 * id for this Patient; this is the primary key
 	 * @var Uuid $patientID
-	 */
+	 **/
 	private $patientId;
 	/**
 	 * id of the Profile that belongs to the patient
 	 *@var Uuid $patientProfileId
-	 */
+	 **/
 	private $patientProfileId;
 	/**
 	 * email of the Patient used when signing up for the site
-	 * @var
-	 */
-	private $patientEmail
+	 * @var string $patientEmail
+	 **/
+	private $patientEmail;
 	/**
-	 *
-	 *
-	 */
-	private $patientUsername
+	 *name given for the Patient on the site that appears when the post to the site.
+	 * @var string $patientUsername
+	 **/
+	private $patientUsername;
 	/**
-	 *
-	 */
-	private $patientInformation
+	 *actual textual content that the Patient will post to their account.
+	 * @var string $patientInformation
+	 **/
+	private $patientInformation;
 	/**
 	 * date and time this Patient post to her account, in a PHP Datetime object
 	 * @var \DateTime $patientDate
+	 **/
+	private $patientDate;
+
+	/**
+	 * constructor for this Patient
 	 */
-	private $patientDate
 }
